ed066d699c3b6921c8fec6e8073e055b194e1f12
Atilla <atillaagungorr@gmail.com>
2023-12-25 20:26:27 +0300

    Project created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import About from "./pages/About/About";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="*" element={<p>Not Found</p>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
